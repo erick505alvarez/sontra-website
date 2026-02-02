d01cb49ade46444c2664b5f3e6e741e2663b3446
Erick Alvarez <erick505alvarez@gmail.com>
2026-02-01 19:37:08 -0500

    add N8N_DEMO_CALL_WEBHOOK variable to github deployment script workflow

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly RESEND_API_KEY: string;
   readonly RESEND_EMAIL_DOMAIN: string;
   readonly TARGET_INBOX: string;
+  readonly N8N_DEMO_CALL_WEBHOOK: string;
   // More env variables...
 }
 
